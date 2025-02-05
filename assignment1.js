9dca0aff08059c80e0060bc7147cd5b2f85d3afd
waelfekih <waaelfkh@gmail.com>
2025-02-05 15:41:58 +0100

    My first push

---
+++ assignment1.js
@@ -0,0 +1,48 @@
+//! Problem 1
+    //console.log(randomCar) = Tesla
+    //console.log(otherRandomCar) = Mercedes
+
+//! Problem 2
+    //console.log(otherName); = Elon
+    //console.log(employeeName); = employeeName is not defined
+
+//! Problem 3
+    //console.log(password); = 12345
+    //console.log(hashedPassword); undefined
+
+//! Problem 4
+    //console.log(first === second); = False
+    //console.log(first === third); = True
+
+//! Problem 5
+    //console.log(key); = value
+    //console.log(secondKey); = [1, 5, 1, 8, 3, 3]
+    //console.log(secondKey[0]); = 1
+    //console.log(willThisWork); = 5
+
+//! Problem 6
+    //Paul was found at index 0
+    //George was found at index 1
+    //John was found at index 2
+    //Ringo was found at index 3
+
+//! Problem 7
+    // NO OUTPUT FOR THIS CODE
+
+//! Problem 8
+    //Paul was found at index 0
+    //George was found at index 1
+    //John was found at index 2
+    //Ringo was found at index 3
+
+//! Problem 9
+    //console.log(planet.composition[0] === planetCopy.composition[0]) = True
+    //console.log(planet === planetCopy) = False
+
+
+
+
+
+
+
+
---
+++ condArrows.js
@@ -0,0 +1,25 @@
+//! Problem 1
+
+const age = 12 ;
+const message = age > 18 ? "You are good to go" : "Sorry! you must be 18 or older"
+
+console.log(message)
+    
+
+//! Problem 2
+
+const is_raining = true ;
+is_raining ? console.log("Get you rain Jacket!") : console.log("No rain on today's forecast")
+
+//! Problem 3
+
+const num = 25 ;
+num % 2 === 0 ? console.log("This is an even number") : console.log("This is an odd Number")
+
+//! Problem 4
+
+const a = 20;
+const b = 21 ;
+a > b ? console.log("a is greater than b") : console.log("a is less than b")
+
+
